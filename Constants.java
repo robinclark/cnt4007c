536043b81a5dc1bd2edda0aa523958ffc50847f0
Nana Amfo <nanaus13@yahoo.com>
2013-10-27 19:36:33 -0400

    added new constants

--- Constants.java
+++ Constants.java
@@ -1,6 +1,16 @@
-public class Constants
-{
-	public static String CFG_FILE = "PeerInfo.cfg";
+public class Constants {
 
+		public static String CFG_FILE  = "PeerInfo.cfg";
+		public static String PDIR = "./";
+		public static int MSG_CHOKE_TYPE = 0;
+		public static int MSG_UNCOKE_TYPE = 1;
+		public static int MSG_INTERESTED_TYPE = 2;
+		public static int MSG_UNINTERESTED_TYPE = 3;
+		public static int MSG_HAVE_TYPE = 4;
+		public static int MSG_BITFIELD_TYPE = 5;
+		public static int MSG_REQUEST_TYPE = 6;
+		public static int MSG_PIECE_TYPE = 7;
+		
 		
 }
+
