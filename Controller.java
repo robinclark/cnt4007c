f145be2a8797f1a88b16a7e3f06050ea13aec86c
Nana Amfo <nanaus13@yahoo.com>
2013-10-27 19:38:09 -0400

    abstracted controller using "module" interface

--- Controller.java
+++ Controller.java
@@ -1,20 +1,50 @@
-public class Controller()
-{
-		private Configuration configFile;
-		//must be synchronized because 
-		public synchronized Controller(int peerID)
-		{
-			configFile = new Configuration(peerID);
-			
+import java.io.IOException;
 
-			
-		}
+public class Controller extends Module {
+	
+	private Configuration configInstance;
+	private Logger logInstance;
+	private String peerID;
+	private Server serverInstance;
+	private List<Peer> neighborPeers;
 
-
-		public void execute()
-		{
-			new Thread(new Server(configFile)).start();
-		}
-
+	
+	@Override
+	public void intialConfiguration() {
 		
+		ModuleFactory factory = new ModuleFactory();
+			if(configInstance == null)
+			{
+				configInstance = (Configuration) factory.createConfigMod();
+				
+			}
+			
+			if(logInstance == null)
+			{
+					logInstance = (Logger) factory.createLogMod(peerID);
+			}
+
+	}
+	
+	public void execute()
+	{
+	  String msg;
+		try {
+			msg = logInstance.testMessage();
+			logInstance.writeLogger(msg);
+			logInstance.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void setPeerID(String peerID)
+	{
+		this.peerID = peerID;
+	}
+	
+	
+
 }
+
