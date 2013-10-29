027294f72e3e376202ca287b6f0c5b0f7862633b
Nana Amfo <nanaus13@yahoo.com>
2013-10-28 22:49:33 -0400

    abstracted normal messages

---
+++ NormalMessage.java
@@ -0,0 +1,8 @@
+public abstract class NormalMessage {
+	
+	public abstract void setMsgLen();
+	public abstract void setMsgType(int type);
+	public abstract void setMsgPayLoad();
+	public abstract void setMsgPayLoad(byte[] payload);
+	
+}
