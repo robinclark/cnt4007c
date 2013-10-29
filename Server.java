6e13957d305e34acf29816184245eb32581a16e9
Nana Amfo <nanaus13@yahoo.com>
2013-10-28 22:56:20 -0400

    removed some broken code
    added serversockets and sockets to create client threads from server
    thread

--- Server.java
+++ Server.java
@@ -1,10 +1,57 @@
-public class Server implements Runnable
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.*;
+
+public class Server extends Module implements Runnable
 {
-	private Configuration configFile;
+	private Configuration configInstance;
+	private String peerID;
+	private Controller controllerInstance;
 	
-	public synchronized PeerServer(Configuration configFile)
+	public Server(String peerID, Module ctrl)
 	{
-		this.configFile = configFile;
-	}	
+		controllerInstance = (Controller) ctrl;
+		this.peerID = peerID;
+	}
+
+	@Override
+	public void intialConfiguration() {
+		if(configInstance == null)
+		{
+			configInstance = (Configuration) ModuleFactory.createConfigMod();
+		}
+		
+	}
+
+	@Override
+	public void run() {
+		
+		HashMap<String,peerInfo> peers = configInstance.getPeerListCollection();
+		
+		int numOfPeers = controllerInstance.getNumberOfConnectedPeers(peerID);
+		peerInfo node = peers.get(peerID);
+		
+		try
+		{
+			ServerSocket peerServer = new ServerSocket(node.getPortNumber());
+			
+			for(int i = 0; i < numOfPeers; i++)
+			{
+				Socket peer = peerServer.accept();
+				
+				Peer neighborPeer = (Peer) ModuleFactory.createPeer(peer, controllerInstance);
+				
+				controllerInstance.addNeighbors(neighborPeer);
+				
+				new Thread(neighborPeer).start();
+			}
+		}catch(IOException e)
+		{
+			
+		}
+		
+		
+	}
 
 }
