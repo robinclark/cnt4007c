10abf490cf702d6877ae551353279e2d38460c10
Nana Amfo <nanaus13@yahoo.com>
2013-10-28 22:53:23 -0400

    made some slight changes to controller
    controller now takes id from a method rather than from constructor, not
    a huge change

--- ModuleFactory.java
+++ ModuleFactory.java
@@ -1,3 +1,5 @@
+import java.net.Socket;
+
 public class ModuleFactory {
 	public static Module createConfigMod()
 	{
@@ -18,11 +20,24 @@ public class ModuleFactory {
 	
 	public static Module createCtrlMod(String peerID)
 	{
-		Controller controller = new Controller();
-		controller.setPeerID(peerID);
+		Controller controller = new Controller(peerID);
 		controller.intialConfiguration();
 		
 		return controller;
+	}
+	
+	public static Module createServerMod(String peerID, Controller controller)
+	{
+		Server server = new Server(peerID, controller);
+		server.intialConfiguration();
+		return server;
+	}
+	
+	public static Module createPeer(Socket socket, Controller controller)
+	{
+		Peer peer = new Peer(socket, controller);
+		peer.intialConfiguration();
+		return peer;
 	}
 
 	public static Module createTopPeerMod()
