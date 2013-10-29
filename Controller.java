0e27c79154c1f76190b1998175151a6fc1fb88c3
Nana Amfo <nanaus13@yahoo.com>
2013-10-28 22:57:41 -0400

    updated controller
    added a method to count the number of connected peers
    added a method to create servers
    added a method to return logger
    added a method to add neighbor peers

--- Controller.java
+++ Controller.java
@@ -1,4 +1,8 @@
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.HashMap;
+import java.util.Set;
 
 public class Controller extends Module {
 	
@@ -8,43 +12,81 @@ public class Controller extends Module {
 	private Server serverInstance;
 	private List<Peer> neighborPeers;
 
+	public Controller(String peerID)
+	{
+		this.peerID = peerID;
+	}
 	
 	@Override
 	public void intialConfiguration() {
-		
-		ModuleFactory factory = new ModuleFactory();
 			if(configInstance == null)
 			{
-				configInstance = (Configuration) factory.createConfigMod();
+				configInstance = (Configuration) ModuleFactory.createConfigMod();
 				
 			}
 			
 			if(logInstance == null)
 			{
-					logInstance = (Logger) factory.createLogMod(peerID);
+					logInstance = (Logger) ModuleFactory.createLogMod(peerID);
+			}
+
+			if(serverInstance == null)
+			{
+					serverInstance = (Server) ModuleFactory.createServerMod(peerID, (Controller) this);
+			}
+			
+			if(neighborPeers == null)
+			{
+				neighborPeers = new ArrayList<Peer>();
 			}
 
 	}
 	
 	public void execute()
 	{
-	  String msg;
-		try {
-			msg = logInstance.testMessage();
-			logInstance.writeLogger(msg);
-			logInstance.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+			createServers();
+			//createClients();
 	}
 	
 	public void setPeerID(String peerID)
 	{
 		this.peerID = peerID;
 	}
+
+	public void createServers()
+	{
+		//new Thread(Server)
+	}
 	
+	public int getNumberOfConnectedPeers(String peerID)
+	{
+		int numOfPeers = 0;
+		
+		HashMap<String, peerInfo> peers = configInstance.getPeerListCollection();
+		
+		Set<String> peerKeys = peers.keySet();
+		
+		for(String peerKey : peerKeys)
+		{
+			if(Integer.parseInt(peerID) < Integer.parseInt(peerKey))
+			{
+				numOfPeers++;
+			}
+		}
+		
+		return numOfPeers;	
+		
+	}
 	
+	public void addNeighbors(Peer peer)
+	{
+		neighborPeers.add(peer);
+	}
+	
+	public Module getLogger()
+	{
+		return logInstance;
+	}
 
 }
 
