4003539e3220d89581ecdc9dca9ab4dbd824c124
Robin <robydee123@gmail.com>
2013-10-31 12:33:16 -0400

    added common.cfg reader & updated constants

--- Constants.java
+++ Constants.java
@@ -1,6 +1,7 @@
 public class Constants {
 
-		public static String CFG_FILE  = "PeerInfo.cfg";
+		public static String PEER_CFG_FILE  = "PeerInfo.cfg";
+                public static String COMMON_CFG_FILE = "Common.cfg";
 		public static String PDIR = "./";
 		public static int MSG_CHOKE_TYPE = 0;
 		public static int MSG_UNCOKE_TYPE = 1;
