9227c6d8c92be70aa46e1e4ca7a5ddbca599b7c7
Nana Amfo <nanaus13@yahoo.com>
2013-10-31 12:03:44 -0400

    changed handshake message to be a serial object

--- HandShakeMessage.java
+++ HandShakeMessage.java
@@ -1,8 +1,71 @@
-public class HandShakeMessage {
-	private byte[] header;
-	private byte[] zero_bits;
-	private byte[] peer_ID;
+import java.io.Serializable;
+
+public class HandShakeMessage extends Message
+{
 	
+	private int msg_type;
+	private String peerID;
+	
+	private static final long serialVersionUID = 3L;
 
+	public long getUID() {
+		return serialVersionUID;
+	}
+	
+	public String getHeader()
+	{
+		return Constants.HANDSHAKE_HEADER;
+	}
+	
+	public void setPeerID(String peerID)
+	{
+		this.peerID = peerID; 
+	}
+	
+	public String getPeerID()
+	{
+		return peerID;
+	}
+	
+	public HandShakeMessage getMessage()
+	{
+		return this;
+	}
 
+	@Override
+	public void setMsgLen() {
+		
+		
+	}
+
+	@Override
+	public void setMsgType(int type) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setMsgPayLoad() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setMsgPayLoad(byte[] payload) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getMsgType() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getMsgLen() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
 }
