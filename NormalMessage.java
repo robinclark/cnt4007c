044278aeede04c4b6ec6710ca19202fb25339886
Nana Amfo <nanaus13@yahoo.com>
2013-10-31 12:04:41 -0400

    removing NormalMessage class
    changing to Message class

--- NormalMessage.java
+++
@@ -1,8 +0,0 @@
-public abstract class NormalMessage {
-	
-	public abstract void setMsgLen();
-	public abstract void setMsgType(int type);
-	public abstract void setMsgPayLoad();
-	public abstract void setMsgPayLoad(byte[] payload);
-	
-}
