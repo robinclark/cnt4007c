04e4f4dd00ca1c26ff9499bc80efa22eb34fda2d
Nana Amfo <nanaus13@yahoo.com>
2013-10-31 12:01:04 -0400

    made the message serial
    doing away with using byte array

--- ChokeMessage.java
+++ ChokeMessage.java
@@ -1,20 +1,19 @@
-import java.nio.ByteBuffer;
 
-public class ChokeMessage extends NormalMessage {
-	private byte[] msg_len;
-	private byte[] msg_type;
+public class ChokeMessage extends Message {
+	private static final long serialVersionUID = 2L;
+	private int msg_len;
+	private int msg_type;
 	private byte[] msg_payload;
-	private byte[] newMsg;
 	
 	
 	@Override
 	public void setMsgLen() {
-		msg_len = ByteBuffer.allocate(4).putInt(msg_type.length).array();
+		msg_len = msg_payload.length;
 	}
 
 	@Override
 	public void setMsgType(int type) {	
-		msg_type = ByteBuffer.allocate(4).putInt(type).array();
+		msg_type = type;
 		
 	}
 
@@ -23,22 +22,37 @@ public class ChokeMessage extends NormalMessage {
 		msg_payload = new byte[4];
 	}
 	
-	
-	public byte[] getMessage()
-	{
-		newMsg = new byte[msg_len.length + msg_type.length + msg_payload.length];
-		System.arraycopy(msg_len, 0, newMsg, 0, msg_len.length);
-		System.arraycopy(msg_type, 0, newMsg, 0, msg_type.length);
-		System.arraycopy(msg_payload, 0, newMsg, 0, msg_payload.length);
-		
-		
-		return newMsg;
-	}
 
 	@Override
 	public void setMsgPayLoad(byte[] payload) {
 		
 		msg_payload = new byte[4];
+	}
+
+	@Override
+	public int getMsgType() {
+		return msg_type;
+	}
+
+	@Override
+	public int getMsgLen() {
+		
+		return msg_len;
+	}
+	
+	public byte[] getMsgPayLoad()
+	{
+		return msg_payload;
+	}
+	
+	public long getUID()
+	{
+		return serialVersionUID;
+	}
+	
+	public Message getMessage()
+	{
+		return this;
 	}
 	
 	
