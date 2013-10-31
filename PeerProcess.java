b46df94a4f3efe24358e731536aa313c7fd56325
Nana Amfo <nanaus13@yahoo.com>
2013-10-31 12:06:52 -0400

    ModuleFactory is static

--- PeerProcess.java
+++ PeerProcess.java
@@ -1,8 +1,7 @@
 public class peerProcess {
 	public static void main(String[] args)
 	{
-		ModuleFactory factory = new ModuleFactory();
-		Module ctrl = factory.createCtrlMod(args[0]);
+		Module ctrl = ModuleFactory.createCtrlMod(args[0]);
 		ctrl.execute();
 	}
 }
