1720177d8c7e7341e253139d9b9ddd3139a1d999
Robin <robydee123@gmail.com>
2013-10-31 15:57:37 -0400

    bitfield manager update

--- BitfieldManager.java
+++ BitfieldManager.java
@@ -1,12 +1,25 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
+import java.util.HashMap;
 
 /**
  *
  * @author Robin
  */
 public class BitfieldManager {
+    HashMap<String, boolean[]> bitFields;
     
+    BitfieldManager()
+    {
+        bitFields = new HashMap<String, boolean[]>();
+    }
+    
+    public void setMap(String peerID, boolean peerBitfield[])
+    {
+        bitFields.put(peerID, peerBitfield.clone());
+    }
+    
+    public void addPiece(String peerID, int index)
+    {
+        bitFields.get(peerID)[index] = true;
+    }
 }
